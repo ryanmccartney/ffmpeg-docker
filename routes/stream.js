1dbb55c1926fe24e86740ba239fb141d022ca8d0
Ryan McCartney <ryan@mccartney.info>
2023-01-04 22:04:31 +0000

    RTMP streaming, text and timecode overlay

--- routes/stream.js
+++ routes/stream.js
@@ -2,11 +2,48 @@
 
 const router = require("express").Router();
 const hashResponse = require("@utils/hash-response");
-const outputSrt = require("@services/output-srt");
+const outputSrtFile = require("@services/output-srt-file");
+const outputSrtBars = require("@services/output-srt-bars");
+const outputRtmpFile = require("@services/output-rtmp-file");
+const outputRtmpBars = require("@services/output-rtmp-bars");
 
 /**
  * @swagger
- * /stream/srt:
+ * /stream/srt/file:
+ *    get:
+ *      description: SRT Stream a file.
+ *      tags: [stream]
+ *      produces:
+ *        - application/json
+ *      responses:
+ *        '200':
+ *          description: Success
+ */
+router.get("/srt/file", async (req, res, next) => {
+    const response = await outputSrtFile(req.body);
+    hashResponse(res, req, { data: response, status: response ? "success" : "error" });
+});
+
+/**
+ * @swagger
+ * /stream/rtmp/file:
+ *    get:
+ *      description: RTMP Stream a file.
+ *      tags: [stream]
+ *      produces:
+ *        - application/json
+ *      responses:
+ *        '200':
+ *          description: Success
+ */
+router.get("/rtmp/file", async (req, res, next) => {
+    const response = await outputRtmpFile(req.body);
+    hashResponse(res, req, { data: response, status: response ? "success" : "error" });
+});
+
+/**
+ * @swagger
+ * /stream/srt/bars:
  *    get:
  *      description: SRT Stream test bars.
  *      tags: [stream]
@@ -16,8 +53,25 @@ const outputSrt = require("@services/output-srt");
  *        '200':
  *          description: Success
  */
-router.get("/srt", async (req, res, next) => {
-    const response = await outputSrt(req.body);
+router.get("/srt/bars", async (req, res, next) => {
+    const response = await outputSrtBars(req.body);
+    hashResponse(res, req, { data: response, status: response ? "success" : "error" });
+});
+
+/**
+ * @swagger
+ * /stream/rtmp/bars:
+ *    get:
+ *      description: RTMP Stream test bars.
+ *      tags: [stream]
+ *      produces:
+ *        - application/json
+ *      responses:
+ *        '200':
+ *          description: Success
+ */
+router.get("/rtmp/bars", async (req, res, next) => {
+    const response = await outputRtmpBars(req.body);
     hashResponse(res, req, { data: response, status: response ? "success" : "error" });
 });
 
