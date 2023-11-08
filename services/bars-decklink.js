4dfa3aa40dc3d392d08994a9ec3eabfbfb0c0d54
Ryan McCartney <ryan@mcartney.info>
2023-11-08 15:05:59 +0000

    Fixed more Input/Output typos

--- services/bars-decklink.js
+++ services/bars-decklink.js
@@ -81,7 +81,7 @@ const process = async (options) => {
         response.errors = [error];
     }
 
-    response.job = await jobManager.get(options.input?.cardName);
+    response.job = await jobManager.get(options.ouput?.cardName);
     return response;
 };
 
