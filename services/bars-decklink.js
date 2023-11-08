9a2a445fc2ec41857f4044e4cc39e649e9d6658c
Ryan McCartney <ryan@mcartney.info>
2023-11-08 15:13:01 +0000

    Typo: "ouput" changed for "output"

--- services/bars-decklink.js
+++ services/bars-decklink.js
@@ -81,7 +81,7 @@ const process = async (options) => {
         response.errors = [error];
     }
 
-    response.job = await jobManager.get(options.ouput?.cardName);
+    response.job = await jobManager.get(options?.output?.cardName);
     return response;
 };
 
