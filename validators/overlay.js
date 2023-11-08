5f9fb1f10f6a7ad83cbf0fe6492d2d02f36d571e
Ryan McCartney <ryan@mccartney.info>
2023-11-08 07:16:22 +0000

    Overlay varaibles made optional

--- validators/overlay.js
+++ validators/overlay.js
@@ -3,23 +3,32 @@
 module.exports = (direction = "input") => {
     return {
         "overlay.line1": {
+            optional: true,
             isString: { default: "", errorMessage: "Line 1 must be a String" },
         },
         "overlay.line2": {
+            optional: true,
             isString: { default: "", errorMessage: "Line 2 must be a String" },
         },
         "overlay.fontSize": {
-            isInt: { min: 10, max: 200, default: 120, errorMessage: "Font size must be between 10 and 200." },
+            optional: true,
+            isInt: { min: 10, max: 200, default: 120, errorMessage: "Font size must be between 10 and 200" },
         },
         "overlay.timecode": {
-            isBoolean: { default: false, errorMessage: "Timecode must be a Boolean value." },
+            optional: true,
+            isBoolean: { default: false, errorMessage: "Timecode must be a Boolean value" },
         },
         "overlay.offset": {
-            isInt: { min: -12, max: 12, default: 0, errorMessage: "Time offset must be between -12 and +12 hours." },
+            optional: true,
+            isInt: { min: -12, max: 12, default: 0, errorMessage: "Time offset must be between -12 and +12 hours" },
         },
         "overlay.font": {
             optional: true,
             isString: { default: "swansea-bold.ttf", errorMessage: "Fonts must be in the fonts folder" },
+        },
+        "overlay.scrolling": {
+            optional: true,
+            isBoolean: { default: false, errorMessage: "Scrolling must be a boolean type" },
         },
     };
 };
