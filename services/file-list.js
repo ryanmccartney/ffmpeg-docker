3ab3a5628fd02502daf28685bcbdf0a1c584a120
Ryan McCartney <ryan@mcartney.info>
2023-11-14 19:03:27 +0000

    More detailed file-list

--- services/file-list.js
+++ services/file-list.js
@@ -7,11 +7,24 @@ const util = require("util");
 
 const readdir = util.promisify(fs.readdir);
 
-module.exports = async (options = { extension: true }) => {
+module.exports = async (options = { extensions: true }) => {
     try {
         const directoryPath = path.join(process.cwd(), "data", "media");
-        const files = await readdir(directoryPath, { withFileTypes: options?.extension });
-        return { files: files };
+        const files = await readdir(directoryPath, { withFileTypes: options?.extensions });
+        const filesDetail = [];
+
+        for (let file of files) {
+            if (file.name !== ".gitkeep") {
+                const stat = await fs.promises.stat(path.join(directoryPath, file.name));
+                filesDetail.push({
+                    name: file.name,
+                    size: stat.size,
+                    ctime: stat.ctime,
+                });
+            }
+        }
+
+        return { files: filesDetail };
     } catch (error) {
         logger.warn(`Cannot list files ${error.message}`);
         return { errors: [error] };
